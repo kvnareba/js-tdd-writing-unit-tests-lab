d310fed423291dd468b48ee58f4564db7c8299e7
kvn <kvnareba@gmail.com>
2024-05-28 06:30:30 +0300

    com

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,20 @@
 // Your code here
+// const isPalindrome = (word) => {
+//     if (!word.trim()) return false;
+//     return word.split("").reverse().join("").toLowerCase() === word.toLowerCase();
+// }
+
+// export { isPalindrome }
+
+
+function reverse(string) {
+    return string.split("").reverse().join("");
+  }
+  
+  export function isPalindrome(word) {
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error("Invalid input");
+    }
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+  }
